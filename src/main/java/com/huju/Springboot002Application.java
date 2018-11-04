73127439163c3ece059fefaf43175ad4489dbf24
huju <13886338732@163.com>
2018-11-04 13:36:25 +0800

    first commit

---
+++ src/main/java/com/huju/Springboot002Application.java
@@ -0,0 +1,12 @@
+package com.huju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot002Application.class, args);
+	}
+}
